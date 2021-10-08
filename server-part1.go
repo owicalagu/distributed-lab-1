06ae16126ca16b6d90ddb673a158fce95c53cbbc
hh20889 <hh20889@bris.ac.uk>
2021-10-08 10:52:02 +0100

    ffff

--- server-part1.go
+++ server-part1.go
@@ -5,14 +5,9 @@ import (
 	"net"
 )
 
-func read() {
-	listener, err := net.Listen("tcp", ":34567")
-	fmt.Println("listening...")
-	if err != nil {
-		fmt.Println("error in listening")
-		return
-	}
+func newConnRead(listener net.Listener) {
 	for {
+		fmt.Println("in server for")
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("error in accepting")
@@ -26,6 +21,34 @@ func read() {
 		fmt.Println("got ", n, "bytes")
 		fmt.Println(string(buf))
 	}
+}
+
+func read() {
+	for {
+
+		listener, err := net.Listen("tcp", ":34567")
+		fmt.Println("listening...")
+		if err != nil {
+			fmt.Println("error in listening")
+			return
+		}
+		go newConnRead(listener)
+	}
+
+	/*for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("error in accepting")
+		}
+		fmt.Println()
+		buf := make([]byte, 4096)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("error in reading")
+		}
+		fmt.Println("got ", n, "bytes")
+		fmt.Println(string(buf))
+	}*/
 }
 
 func main() {
