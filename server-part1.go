200dcb386959d5f22afb5b12109e2ea5b066b6ea
hh20889 <hh20889@bris.ac.uk>
2021-10-08 10:00:19 +0100

    reader

--- server-part1.go
+++ server-part1.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+)
 
+func read() {
+	listener, err := net.Listen("tcp", ":8030")
+	if err != nil {
+		fmt.Println("error in listening")
+		return
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("error in accepting")
+		}
+		buf := make([]byte, 4096)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("error in reading")
+		}
+		fmt.Println("got ", n, "bytes")
+		fmt.Println(string(buf))
+	}
+}
+
+func main() {
+	read()
 }
