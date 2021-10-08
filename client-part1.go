a297015fac5a9aed838acd772467a4783276cff9
hh20889 <hh20889@bris.ac.uk>
2021-10-08 10:37:12 +0100

    reader

--- client-part1.go
+++ client-part1.go
@@ -6,9 +6,10 @@ import (
 )
 
 func connect() {
-	conn, err := net.Dial("tcp", "172.31.95.134:8030")
+	conn, err := net.Dial("tcp", "172.31.95.134:8010")
 	if err != nil {
 		fmt.Println("error in connection")
+		fmt.Println(err)
 		return
 	}
 	_, _ = fmt.Fprintf(conn, "hi")
--- server-part1.go
+++ server-part1.go
@@ -6,7 +6,7 @@ import (
 )
 
 func read() {
-	listener, err := net.Listen("tcp", ":8030")
+	listener, err := net.Listen("tcp", ":8010")
 	if err != nil {
 		fmt.Println("error in listening")
 		return
